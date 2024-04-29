fb70cfc6758832e580a1c142b75d82b482c93d3a
tiagorafael404 <161669844+tiagorafael404@users.noreply.github.com>
2024-04-29 19:02:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,18 @@ function acceptCookies() {
     document.getElementById('cookieConsent').style.display = 'none';
 }
 
-// Função para exibir a mensagem de consentimento de cookies se ainda não tiver sido aceita após 5 minutos
+// Função para exibir a mensagem de consentimento de cookies se ainda não tiver sido aceita após 1 mês
 function showCookieConsent() {
     if (!areCookiesAccepted()) {
         var lastAccepted = localStorage.getItem('lastAccepted');
-        var fiveMinutesAgo = new Date().getTime() - (5 * 60 * 1000); // Calcula o tempo 5 minutos atrás em milissegundos
-        if (!lastAccepted || lastAccepted < fiveMinutesAgo) {
+        var oneMonthAgo = new Date().getTime() - (30 * 24 * 60 * 60 * 1000); // Calcula o tempo 1 mês atrás em milissegundos
+        if (!lastAccepted || lastAccepted < oneMonthAgo) {
             document.getElementById('cookieConsent').style.display = 'block';
         }
     }
 }
 
+// Exibir a mensagem de consentimento de cookies quando a página carregar
+window.onload = function() {
+    showCookieConsent();
+};
