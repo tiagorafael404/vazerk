45558e85edf67d0c508814fc40fdaf6a07f59bad
tiagorafael404 <161669844+tiagorafael404@users.noreply.github.com>
2024-12-05 21:27:55 +0000

    a

--- script.js
+++ script.js
@@ -2,21 +2,16 @@ const cookieBox = document.querySelector(".cookies-box"),
   buttons = document.querySelectorAll(".button");
 
 const executeCodes = () => {
-  //if cookie contains codinglab it will be returned and below of this code will not run
   // Verifica se o cookie "codinglab" já existe
   if (document.cookie.includes("codinglab")) return;
-  cookieBox.classList.add("show");
 
-  cookieBox.classList.add("show"); // Mostra a caixa de cookies
+  // Mostra a caixa de cookies
+  cookieBox.classList.add("show");
 
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
+      // Oculta a caixa de cookies
       cookieBox.classList.remove("show");
-      //if button has acceptBtn id
-      if (button.id == "acceptBtn") {
-        //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
-        document.cookie = "cookieBy= codinglab; max-age=" + 60 * 60 * 24 * 30;
-      cookieBox.classList.remove("show"); // Oculta a caixa de cookies
 
       // Verifica se o botão clicado é o de aceitar
       if (button.id === "acceptBtn") {
@@ -26,12 +21,9 @@ const executeCodes = () => {
     });
   });
 };
-//executeCodes function will be called on webpage load
 
 // Chama a função quando a página carrega
 window.addEventListener("load", executeCodes);
-
-
 
 
 
